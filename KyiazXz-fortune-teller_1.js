50e0bb3578b4e8c78f469b4498965b9e0424f8c2
KYIAZBEK ZHOLDOSHEV <111270349+KyiazXz@users.noreply.github.com>
2022-10-19 01:11:42 +0600

    header

---
+++ KyiazXz-fortune-teller_1.js
@@ -0,0 +1,12 @@
+// Store the following into variables: number of children, partner's name, geographic location, job title.
+// Output your fortune to the screen like so: "You will be a X in Y, and married to Z with N kids."
+
+
+let numberOfChildren = 12;
+let partnersName='Aziz';
+let geoLocation='USA';
+let jobTitle='Developer';{
+console.log ("You will be a "+ jobTitle + " in " + geoLocation + ", and married to"+ partnersName +" with " +numberOfChildren + "kids");
+}
+
+
